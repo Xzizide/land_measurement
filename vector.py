a93b4c4b096aca17e671752469c3abdb6d4b24b0
Emil Andersson <emilius.andersson10@gmail.com>
2024-10-12 23:28:55 +0200

    Added the direction variable for vector describing the vector

--- vector.py
+++ vector.py
@@ -6,5 +6,12 @@ class Vector:
         self.initial_point = initial_point
         self.terminal_point = terminal_point
 
+        self.direction = []
+        initial_coords, terminal_coords = self.initial_point.equal_amount_coords(
+            self.terminal_point
+        )
+        for index, coordinate in enumerate(initial_coords):
+            self.direction.append(terminal_coords[index] - coordinate)
+
     def magnitude(self) -> float:
         return self.initial_point.distance_from(self.terminal_point)
